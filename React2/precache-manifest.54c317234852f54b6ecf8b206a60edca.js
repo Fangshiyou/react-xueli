6a420b105772f90a557fdce2f436a4cedab3bf0f
房世有 <33642994+Fangshiyou@users.noreply.github.com>
2020-10-18 17:25:56 +0800

    Add files via upload

---
+++ React2/precache-manifest.54c317234852f54b6ecf8b206a60edca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2461bc716875eadfe3cf7660f5c2cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9666336c38722f2036",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "8132eead7d11c6bac0d7",
+    "url": "/static/css/main.a3d6af22.chunk.css"
+  },
+  {
+    "revision": "bc9666336c38722f2036",
+    "url": "/static/js/2.0069c370.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0069c370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8132eead7d11c6bac0d7",
+    "url": "/static/js/main.e2fe306c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);

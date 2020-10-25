f0ea8e40b3c60718a7dd8c1ccfed99dc4fca0520
房世有 <33642994+Fangshiyou@users.noreply.github.com>
2020-10-25 19:29:10 +0800

    Add files via upload

---
+++ taobao/precache-manifest.aa29fa7e2f9507dc11a31e17846b923c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5731da03b8897df668420cce5ccc1fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f81663028cfba4f589",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b7c208066c18c26d7771",
+    "url": "/static/css/main.59854acf.chunk.css"
+  },
+  {
+    "revision": "e1f81663028cfba4f589",
+    "url": "/static/js/2.1e728870.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "/static/js/2.1e728870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c208066c18c26d7771",
+    "url": "/static/js/main.9ca0bd3e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);

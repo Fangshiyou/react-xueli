a8e2586b20731f153f5772c53c9c1031d1f9af4f
Fangshiyou <1244841562@qq.com>
2020-11-04 21:25:49 +0800

    tao

---
+++ taobao/precache-manifest.117fec6668dccfec6c15d275c1f6b4bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba4ad703be3719dacaa3e1c843812b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "980dae19073aa6425764",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "e3df3e2f9cfcd3783a17",
+    "url": "./static/css/main.a710178e.chunk.css"
+  },
+  {
+    "revision": "980dae19073aa6425764",
+    "url": "./static/js/2.83cf19a3.chunk.js"
+  },
+  {
+    "revision": "5039e6fb7ed7bc0c7b07b989bcfe237c",
+    "url": "./static/js/2.83cf19a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3df3e2f9cfcd3783a17",
+    "url": "./static/js/main.45161241.chunk.js"
+  },
+  {
+    "revision": "03339e00c59c982f3143",
+    "url": "./static/js/runtime-main.b4adcb0b.js"
+  },
+  {
+    "revision": "16b59dd9ce453f0e4c3ee543d21a141c",
+    "url": "./static/media/iconfont.16b59dd9.ttf"
+  },
+  {
+    "revision": "35a7392b2516502d4f2baedc0d4c614c",
+    "url": "./static/media/iconfont.35a7392b.eot"
+  },
+  {
+    "revision": "9ba2fa3772aba6eb673a1c09b7f88240",
+    "url": "./static/media/iconfont.9ba2fa37.svg"
+  },
+  {
+    "revision": "bfb0beeb0891a82c524f4669a7b5e618",
+    "url": "./static/media/iconfont.bfb0beeb.woff"
+  }
+]);
